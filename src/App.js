ed8e79ff7710f829d9a53fa0ccecfed1df01f389
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-25 20:26:57 -0500

    P

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      number:0
+    }
+  }
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.number}</span>
+        <button id="inc" onClick={this.addOne.bind(this)}>Incrementa</button>
       </div>
     );
+  }
+  addOne(){
+    this.setState({      
+      number : this.state.number + 1
+    });
   }
 }
 
